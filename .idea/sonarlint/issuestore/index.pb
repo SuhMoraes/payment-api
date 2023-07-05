dc00520b25a8d9a0af9d46a52ddaecd2f1f097e5
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-07-05 11:11:41 -0300

    Docker compose created, config Producer Factory Message Kafka

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,9 @@
 
-Ä
-Pstr-producer/src/main/java/com/suhmoraes/strproducer/StrProducerApplication.java,b\5\b54f94aa2afbc35e9153192ca3709221d63cb81a
+Å
+Qstr-producer/src/main/java/com/suhmoraes/strproducer/config/KafkaAdminConfig.java,a\8\a87f8ca01a23384fc4e5038f17aee5c776566ec0
 D
 str-producer/gradlew,8\3\83ae5226f13df1386b2d6f56552581c6051e0573
 I
 str-producer/build.gradle,7\1\715e507a5020658219efff605d5d976cf842e341
+Ä
+Pstr-producer/src/main/java/com/suhmoraes/strproducer/StrProducerApplication.java,b\5\b54f94aa2afbc35e9153192ca3709221d63cb81a
---
+++ str-producer/src/main/java/com/suhmoraes/strproducer/resources/StringProducerResources.java
@@ -0,0 +1,26 @@
+package com.suhmoraes.strproducer.resources;
+
+import com.suhmoraes.strproducer.services.StringProducerService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/producer")
+public class StringProducerResources {
+
+    private final StringProducerService producerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+}
